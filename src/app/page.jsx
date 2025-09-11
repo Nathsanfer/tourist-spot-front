3c46ec7d4c1d37fd3f1705f6540beb24d5f2ac75
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-09-11 00:27:25 -0300

    feat: create Hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import styles from "./page.module.css";
 
+import Image from "next/image";
+
 import Header from "../components/Header/page.jsx";
 
 export default function Home() {
@@ -7,6 +9,34 @@ export default function Home() {
     <div>
 
       <Header />
+
+      <main className={styles.heroContainer}>
+        <section className={styles.heroContent}>
+          <div className={styles.heroText}>
+            <h1>
+              Conheça e explore<br />
+              lugares mágicos<br />
+              do <span className={styles.brasil}>Brasil</span>!!
+            </h1>
+            <p className={styles.heroDescription}>
+              Monte sua bagagem e embarque nessa aventura. Venha conosco viajar pelo Brasil afora em busca de paisagens deslumbrantes e lugares incríveis para conhecer
+            </p>
+            <button className={styles.heroButton}>
+              Venha explorar com a gente!
+            </button>
+          </div>
+          <div className={styles.heroImages}>
+            <div className={styles.heroTopImages}>
+              <Image src="/images/hero1.png" alt="Cristo Redentor" width={380} height={260} className={styles.heroImg} />
+              <Image src="/images/hero4.png" alt="Avião" width={320} height={200} className={styles.heroImg} />
+            </div>
+            <div className={styles.heroBottomImages}>
+              <Image src="/images/hero2.png" alt="Cataratas do Iguaçu" width={380} height={220} className={styles.heroImg} />
+              <Image src="/images/hero3.png" alt="Cachoeira" width={290} height={220} className={styles.heroImg} />
+            </div>
+          </div>
+        </section>
+      </main>
       
     </div>
   );
